bc1569fa04ca8a010d849e93a824a158962abe70
supernovamiyu <rojascamposyinet1101@gmail.com>
2024-12-08 12:58:20 -0500

    Se crea el archivo view.js para desplegar el contenido de las plantillas dentro de "inicio.html".

---
+++ frontend/view/js/view.js
@@ -0,0 +1,18 @@
+class Vista {
+    constructor(){}
+
+    /**
+     *  Despliega el contenido de una plantilla en el destino correspondiente
+     * @param {str} plantilla :id de la plantilla de origen
+     * @param {str} destino :id de la etiqueta de destino
+     */
+    mostrarPlantilla(plantilla, destino){
+        const template = document.getElementById(plantilla);
+        const clone = document.importNode(template.content, true);
+        const dest = document.getElementById(destino);
+        if (template && dest) {
+            dest.innerHTML="";
+            dest.appendChild(clone);
+        }
+    }
+}
