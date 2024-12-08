3cd823aaaf0c1c2f61eb0e25a43e414bf63f8b40
supernovamiyu <rojascamposyinet1101@gmail.com>
2024-12-08 12:52:44 -0500

    Se realizan las funciones para cada una de las plantillas definidas en "inicio.html" de DollarSoftware.

---
+++ frontend/controller/controller.js
@@ -0,0 +1,31 @@
+
+let vista = null;
+
+window.onload = function(){
+    vista = new Vista();
+    // Mostrar la página inicial
+    vista.mostrarPlantilla("plantilla-inicio", "container-2")
+
+}
+
+function mostrarPantallaSesion(event) {
+    event.preventDefault();
+    vista.mostrarPlantilla("plantilla-sesion", "container-2")
+    
+}
+
+function mostrarPantallaCarrito(event) {
+    event.preventDefault();
+    vista.mostrarPlantilla("plantilla-carrito", "container-2")
+    
+}
+
+function mostrarPantallaInicio(event) {
+    event.preventDefault();
+    vista.mostrarPlantilla("plantilla-inicio", "container-2")
+}
+
+function mostrarPantallaAtencionCliente(event){
+    event.preventDefault();
+    vista.mostrarPlantilla("plantilla-atencion-cliente", "container-2")
+}
