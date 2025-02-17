c4dd88390f32f6eac3bc58e4188da03e44095f53
supernovamiyu <rojascamposyineth01@gmail.com>
2025-02-17 09:17:18 -0500

    Modificación de la conexión a la base de datos para poder trabajar con ella desde el SENA

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,10 +1,27 @@
+
+// PARA TRABAJAR CON LA BASE DE DATOS LOCAL
+
 const mysql2 = require('mysql2');
 
+// Conexion a base de datos
+//const databaseConnection = mysql2.createConnection({
+    //host: 'localhost',
+    //user:  'root',
+    //password: 'Yinet2006@@ñ',
+    //database: 'db_dollarsoftware',
+    //port: 3306,
+//});
+
+//module.exports = databaseConnection;
+
+
+
+/// PARA TRABAJAR CON LA BASE DE DATOS EN EL SENA
 // Conexion a base de datos
 const databaseConnection = mysql2.createConnection({
     host: 'localhost',
     user:  'root',
-    password: 'Yinet2006@@ñ',
+    password: 'Hola1234@1234',
     database: 'db_dollarsoftware',
     port: 3306,
 });
