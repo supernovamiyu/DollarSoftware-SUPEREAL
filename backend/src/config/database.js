6bf85ab189e4ee2d01509c79f3af623fbbeda717
supernovamiyu <rojascamposyineth01@gmail.com>
2025-02-26 08:44:55 -0500

    Implementación de variables de entorno.

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -4,24 +4,10 @@
 const mysql2 = require('mysql2');
 
 // Conexion a base de datos
-//const databaseConnection = mysql2.createConnection({
-    //host: 'localhost',
-    //user:  'root',
-    //password: 'Yinet2006@@ñ',
-    //database: 'db_dollarsoftware',
-    //port: 3306,
-//});
-
-//module.exports = databaseConnection;
-
-
-
-/// PARA TRABAJAR CON LA BASE DE DATOS EN EL SENA
-// Conexion a base de datos
 const databaseConnection = mysql2.createConnection({
     host: 'localhost',
     user:  'root',
-    password: 'Hola1234@1234',
+    password: process.env.DB_PASSWORD || 'Hola1234@1234',
     database: 'db_dollarsoftware',
     port: 3306,
 });
