431a1fd581eebc7bfc3c85239423bfff1ee5af5c
supernovamiyu <rojascamposyineth01@gmail.com>
2025-02-25 20:45:40 -0500

    Modificación del código JS para el carrusel.

--- frontend/view/js/carousel.js
+++ frontend/view/js/carousel.js
@@ -1,17 +1,26 @@
-let slideIndex = 1;
-        showSlides(slideIndex);
+let slideIndex = 1
+showSlides(slideIndex)
 
-        function changeSlide(n) {
-            showSlides(slideIndex += n);
-        }
+function changeSlide(n) {
+    showSlides((slideIndex += n))
+}
 
-        function showSlides(n) {
-            let i;
-            let slides = document.getElementsByClassName("carousel-slide");
-            if (n > slides.length) {slideIndex = 1}
-            if (n < 1) {slideIndex = slides.length}
-            for (i = 0; i < slides.length; i++) {
-                slides[i].style.display = "none";
-            }
-            slides[slideIndex-1].style.display = "block";
-        }
+function showSlides(n) {
+    let i
+    const slides = document.getElementsByClassName("carousel-slide")
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"
+    }
+    slides[slideIndex - 1].style.display = "block"
+}
+
+// Asegúrate de que las diapositivas se muestren correctamente al cargar la página
+document.addEventListener("DOMContentLoaded", () => {
+    showSlides(slideIndex)
+})
