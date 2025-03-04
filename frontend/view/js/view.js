2b0d2fd5d1611e6043f26e608c1038450b69ae7d
supernovamiyu <rojascamposyineth01@gmail.com>
2025-03-04 09:42:22 -0500

    Añadiendo estilos y probando rutas para los productos.

--- frontend/view/js/view.js
+++ frontend/view/js/view.js
@@ -16,3 +16,55 @@ class Vista {
         }
     }
 }
+
+fetch('/products/')
+    .then(response => response.json())
+    .then(data => {
+        const productosContenedor = document.getElementById('productos-populares');
+
+        data.forEach(producto => {
+            const productoHTML = `
+        <div>
+          <h3>${producto.nombre_producto}</h3>
+          <p>Precio: $${producto.precio}</p>
+          <p>Stock: ${producto.unidades_disponibles}</p>
+          <button class="comprar" data-id="${producto.id_productos}" data-unidades-disponibles="${producto.unidades_disponibles}">Comprar</button>
+        </div>
+      `;
+
+            productosContenedor.insertAdjacentHTML('beforeend', productoHTML);
+        });
+
+        // Agrega evento de clic a los botones de comprar
+        const comprarBotones = document.querySelectorAll('.comprar');
+
+        comprarBotones.forEach(boton => {
+            boton.addEventListener('click', function () {
+                const id = this.getAttribute('data-id');
+                let unidadesDisponibles = parseInt(this.getAttribute('data-unidades-disponibles'));
+
+                if (unidadesDisponibles > 0) {
+                    // Actualiza el stock en el backend
+                    fetch(`/products/${id}`, {
+                        method: 'PUT',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify({ unidades_disponibles: unidadesDisponibles - 1 })
+                    })
+                        .then(response => response.json())
+                        .then(data => {
+                            console.log(data);
+                            // Actualiza el stock en la interfaz
+                            unidadesDisponibles--;
+                            this.setAttribute('data-unidades-disponibles', unidadesDisponibles);
+                            const stockElement = this.parentNode.querySelector('p:nth-child(3)');
+                            stockElement.textContent = `Stock: ${unidadesDisponibles}`;
+                        });
+                } else {
+                    alert('No hay stock disponible para este producto.');
+                }
+            });
+        });
+    });
+
