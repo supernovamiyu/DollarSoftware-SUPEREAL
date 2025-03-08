49f71ec268dfc4cb5156ca9bacf024c2a03c707c
supernovamiyu <rojascamposyineth01@gmail.com>
2025-03-08 14:57:52 -0500

    Corrigiendo la alineación de los elementos de la barra de inicio.

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -4,8 +4,8 @@ const mysql2 = require('mysql2');
 const databaseConnection = mysql2.createConnection({
     host: 'localhost',
     user:  'root',
-    // password: "Yinet2006@@ñ",
-    password: 'Hola1234@1234', 
+    password: "Yinet2006@@ñ",
+    // password: 'Hola1234@1234', 
     database: 'db_dollarsoftware',
     port: 3306,
 });
