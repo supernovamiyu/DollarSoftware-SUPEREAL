0b51755f149e560842949a145064d250eff1ba94
supernovamiyu <rojascamposyineth01@gmail.com>
2025-03-16 10:18:12 -0500

    Modificando el nombre del archivo de conexión al servidor y la ruta de acceso en package.json

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -4,7 +4,7 @@ const mysql2 = require('mysql2');
 // Conexion a base de datos
 const databaseConnection = mysql2.createConnection({
     host: process.env.DB_HOST || 'localhost',
-    user:  process.envDB_USER || 'root',
+    user:  process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || 'Yinet2006@@ñ',
     database: process.env.DB_NAME || 'db_dollarsoftware',
     port: process.env.DB_PORT || 3306,
